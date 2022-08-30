de82eee6b13974e25948dae45a9c36a5a21fb7bb
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-08-30 23:32:48 +0900

    test_cache.py: test cache.py

---
+++ tests/test_cache.py
@@ -0,0 +1,11 @@
+# tests/test_cache.py
+from pathlib import Path
+
+from src.cache import Cache
+
+PATH = Path(".") / ".cache" / "dict"
+
+
+def test_cache_path():
+    cache_path = Cache.cache_path()
+    assert str(cache_path) == str(PATH.resolve())
