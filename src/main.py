982a222e5229bfd3cd95b1d84cb9ea67cac7f84f
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-08-30 23:33:00 +0900

    main.py: add cache flow

--- src/main.py
+++ src/main.py
@@ -1,12 +1,22 @@
 # src/main.py
 
 from api import fetch_html_from_weblio
+from cache import Cache
 from parsing import parsing
 
 
 def main():
     input_words = parsing()
-    html = fetch_html_from_weblio(input_words)
+    Cache.create_dir()
+
+    if not Cache.find_path(input_words):
+        html = fetch_html_from_weblio(input_words)
+        Cache.create_cache(input_words, html)
+        print("create cache")
+        return
+
+    html = Cache.read_cache(input_words)
+    print("use cache")
     print(html)
 
 
