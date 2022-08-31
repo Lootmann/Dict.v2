2f84fa90c4cc96b393e91b54e4af5693aae110a7
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-09-01 02:25:55 +0900

    main.py: change create cache flow

--- src/main.py
+++ src/main.py
@@ -12,20 +12,21 @@ def main():
     if not Cache.find_path(input_words):
         html = fetch_html_from_weblio(input_words)
         scraper = Scraper(html=html)
-        Cache.create_cache(input_words, str(scraper))
+
         print("create cache")
-        print(str(scraper)[:20])
+
+        if scraper.exists:
+            Cache.create_cache(input_words, str(scraper))
+            print(str(scraper)[:20])
+        else:
+            Cache.create_cache(input_words, str("not found"))
+            print("not found")
+
         return
 
     print("use cache")
     cache_file = Cache.read_cache(input_words)
-
-    scraper = Scraper(html=cache_file)
-    if not scraper.exists:
-        print(f"{input_words} not found :^)")
-        return
-
-    print(scraper.get_headword())
+    print(cache_file[:20])
 
 
 if __name__ == "__main__":
