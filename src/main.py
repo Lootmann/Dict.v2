f17654bbede9868601dbf10e9a438bdd78f883e2
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-08-31 23:10:39 +0900

    main.py: tmp change

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
 # src/main.py
-
 from api import fetch_html_from_weblio
 from cache import Cache
 from parsing import parsing
-from scraping import scraping
+from scraping import Scraper
 
 
 def main():
@@ -12,15 +11,21 @@ def main():
 
     if not Cache.find_path(input_words):
         html = fetch_html_from_weblio(input_words)
-        scraped = scraping(html)
-        Cache.create_cache(input_words, scraped)
+        scraper = Scraper(html=html)
+        Cache.create_cache(input_words, str(scraper))
         print("create cache")
-        print(scraped.split("\n")[:3])
+        print(str(scraper)[:20])
         return
 
-    scraped = Cache.read_cache(input_words)
     print("use cache")
-    print(scraped.split("\n")[:3])
+    cache_file = Cache.read_cache(input_words)
+
+    scraper = Scraper(html=cache_file)
+    if not scraper.exists:
+        print(f"{input_words} not found :^)")
+        return
+
+    print(scraper.get_headword())
 
 
 if __name__ == "__main__":
