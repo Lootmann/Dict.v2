9900b0e42e2040954c805c21175b6cd44c4867cc
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-09-01 02:20:37 +0900

    scraping.py: fix Scraping.exists() impl

--- src/scraping.py
+++ src/scraping.py
@@ -12,14 +12,9 @@ class Scraper:
     def __init__(self, html: str):
         """
         NOTE:
-            self._soup で必要不可欠な要素だけを取得
-            そうすると 'self._exists' で anoOnnanoko が取得できなくなるため
-            やむなくこういった実装にしている
-
-        FIXME:
-            exists() を scraping の前処理で実施して False だったら
-            以降の処理を全部省くというのもあり というかそっちのほうがよさそう
-            すべての関数に self._exists を書いていくのは明らかに実装がおかしい
+            必要な要素は id="main" 配下に一つを除いてすべて存在している
+            ただ単語がWeblioに存在しているかどうかは id="anoOnnanoko" で確認する必要があり
+            id="main" の範囲外に存在しているため こういった実装にしている
 
         @param html: fetched Weblio html
         """
@@ -35,18 +30,18 @@ class Scraper:
         """
         get headword class="midashigo"
 
-        @return: str
+        @return: str - headword
         """
-        if not self.exists:
-            return ""
-
         headword = self._soup.find(class_="midashigo")
         return unicodedata.normalize("NFKC", headword.get_text().strip())
 
     def get_description(self) -> str:
-        if not self.exists:
-            return ""
+        """
+        get word description
+        <span> class="content-explanation ej"
 
+        @return: str - description
+        """
         description = self._soup.find(class_="content-explanation ej")
         return description.get_text().strip()
 
